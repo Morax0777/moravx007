f5814d23b348cb20cadc91078505f6acd4bede77
Morax0777 <66309010061@cmtc.ac.th>
2024-06-20 13:48:41 +0700

    new commit

--- app/about/page.js
+++ app/about/page.js
@@ -12,8 +12,8 @@ export default function about() {
                         alt="Card Image"></img>
                     <div class="card-body text-center">
                         <h5 class="card-title">เกมแนะนำ</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
+                        <p class="card-text">kuy i Aom
+                            .</p>
                         <a href="#" class="btn btn-primary">Button</a>
                     </div>
                 </div>
